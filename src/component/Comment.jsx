6920d8bd395b8214f98296a60b9e85e8ca36d737
khokan <khokancrmt@gmail.com>
2025-02-12 17:54:59 +0600

    update comment

--- src/component/Comment.jsx
+++ src/component/Comment.jsx
@@ -5,7 +5,7 @@ const Comment = ({cmt}) => {
     return (
         <div className='my-4 space-y-2'>
             <div className='flex gap-2 items-center'>
-                <div>
+                <div className=''>
                     <img className='w-12 h-12 rounded-full' src={photo} alt="" />
                 </div>
                 <div className='text-xl font-semibold'>{name}</div>
